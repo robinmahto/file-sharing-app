db40dcbb02b55a9eb9a083c3203bdcd1fe104772
robinmahto <robinmahto413@gmail.com>
2023-02-14 23:49:20 +0530

    worked with multer to store images and save file path in db

--- server/controllers/fileContoller.js
+++ server/controllers/fileContoller.js
@@ -5,44 +5,45 @@ import { FileModels } from '../models';
 import { APP_BASE_URL } from '../config';
 
 const storage = multer.diskStorage({
-    destination : (req, file, cb)=>{
+    destination: (req, file, cb) => {
         cb(null, 'uploads/')
     },
-    filename : (req, file, cb)=>{
-       const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
-       cb(null, uniqueName);
+    filename: (req, file, cb) => {
+        const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${path.extname(file.originalname)}`;
+        cb(null, uniqueName);
     }
 })
 
 const upload = multer({
     storage,
-    limits : {fileSize : 1000000 * 100}
+    limits: { fileSize: 1000000 * 100 }
 }).single('file');
 
-const fileController = (req, res, next)=>{
-      // validate
-     if(!req.file){
-       return res.json({error: 'All fields are required'});
-     }
+const fileController = (req, res, next) => {
     //   store file
-    upload(req, res, async(error)=>{
-        if(error){
-            return res.status(500).json({error: error.message})
+    upload(req, res, async (error) => {
+        // validate
+        if (!req.file) {
+            return res.json({ error: 'All fields are required' });
         }
-     // store file into database
-     const file = new FileModels({
-        filename : req.file.filename,
-        uuid: uuidv4(),
-        path: req.file.path,
-        size: req.file.size
-     })
 
-     const response = await file.save();
-     return res.json({files: `${APP_BASE_URL}/files/${response.uuid}`})
+        if (error) {
+            return res.status(500).json({ error: error.message })
+        }
+        // store file into database
+        const file = new FileModels({
+            filename: req.file.filename,
+            uuid: uuidv4(),
+            path: req.file.path,
+            size: req.file.size
+        })
+
+        const response = await file.save();
+        return res.json({ files: `${APP_BASE_URL}/files/${response.uuid}` })
 
     })
 
-   
+
     // response
 }
 
